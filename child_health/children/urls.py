1d2bb7fd1b78ff37dda45f06d2f4f0b9d9028d2b
SaniIbrahim <70323486+SaniIbrahim@users.noreply.github.com>
2022-08-25 00:17:34 +0100

    Update urls.py

--- child_health/children/urls.py
+++ child_health/children/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import ChildrenList,ChildrenDetail,ChildrenRegister,ChildrenUpdate,ChildrenDelete,AntegenDetail,AntegenAdd
-
+,AntegenDelete,AntegenUpdate
 
 
 urlpatterns = [
